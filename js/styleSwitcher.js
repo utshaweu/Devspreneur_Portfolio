b19189ce257503b9774808eb7720e0cd6d520ec7
utshaweu <mahmudsajib95@gmail.com>
2020-06-26 19:01:21 +0600

    dark mode added

--- js/styleSwitcher.js
+++ js/styleSwitcher.js
@@ -1,10 +1,10 @@
 const links = document.querySelectorAll(".alternate-style"),
       totalLinks = links.length;
 
-      
+
 function setActiveStyle(color){
   for(let i = 0; i<totalLinks; i++){
-    if(color == links[i].getAttribute("title")){
+    if(color === links[i].getAttribute("title")){
       links[i].removeAttribute("disabled");
     }
     else{
@@ -12,3 +12,23 @@ function setActiveStyle(color){
     }
   }
 }
+
+
+//Body Skin
+const bodySkin = document.querySelectorAll(".body-skin"),
+      totalBodySkin = bodySkin.length;
+  for(let i = 0; i<totalBodySkin; i++){
+    bodySkin[i].addEventListener("change",function(){
+      if(this.value === "dark"){
+        document.body.className="dark";
+      }
+      else{
+        document.body.className="";
+      }
+    })
+  }    
+
+
+document.querySelector(".toggle-style-switcher").addEventListener("click", () => {
+  document.querySelector(".style-switcher").classList.toggle("open");
+})
